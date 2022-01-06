aabb0e986c6e0c3d936f294406a74b3550d87e33
soponpu <97213248+soponpu@users.noreply.github.com>
2022-01-06 15:52:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,15 @@
-import streamlit as st
 import cv2
-st.write("""# Hello Sopon""")
-FRAME_WINDOW = st.image([])
+import streamlit as st
 
+st.write("""# Hello Sopon""")
+run = st.checkbox('Run')
+FRAME_WINDOW = st.image([])
 camera = cv2.VideoCapture(0)
 #img = cv2.imread("Aeroplane.jpg")
 #img = img[:,:,::-1]
-while (camera.isOpened()):
-    _, frame = camera.read()
-    
+while (run):
+    ret, frame = camera.read()    
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    st.image(frame)
-    #FRAME_WINDOW.image(frame)
-#else:
-    #st.write('Stopped')
+    FRAME_WINDOW.image(frame)
+else:
+    st.write('Stopped')
